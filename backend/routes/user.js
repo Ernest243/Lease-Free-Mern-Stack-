5e042b2bdb70039e9bee6b3919c2fdb8e2f4d67e
jwilliams <jonathan.williams2205@gmail.com>
2020-04-26 11:43:27 -0400

    fixed password (tested with postman)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -36,35 +36,47 @@ router.route('/signUp').post((req, res) => {
       });
 });
 
-router.post('/login').post((req, res) =>{
+router.route('/login').post((req, res) =>{
     const email = req.body.email;
     const password = req.body.password;
-
-    User.findOne({email}).then(user =>{
-        if(!user){
-            return res.status(404).json({emailnotfound: "Email not found"});
+  
+    // Find user by email
+    User.findOne({ email }).then(user => {
+      // Check if user exists
+      if (!user) {
+        return res.status(404).json({ emailnotfound: "Email not found" });
+      }
+  
+      // Check password
+      bcrypt.compare(password, user.password1).then(isMatch => {
+        if (isMatch) {
+          // User matched
+          // Create JWT Payload
+          const payload = {
+            id: user.id,
+            name: user.name
+          };
+  
+          // Sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            {
+              expiresIn: 31556926 // 1 year in seconds
+            },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        } else {
+          return res
+            .status(400)
+            .json({ passwordincorrect: "Password incorrect" });
         }
-
-        bcrypt.compare(password, user.password).then(isMatch =>{
-            if(isMatch){
-                const payload = {
-                    id : user.id,
-                    name : user.firstName
-                };
-
-                jwt.sign(
-                    payload,
-                    keys.secretOrKey,
-                    {expiresIn: 60*60*1000*2},
-                    (err,token)=>{
-                        res.json({success: true, token: "Bearer "+ token})
-                    }
-                );
-            }
-            else{
-                return res.status(400).json({passwordincorrect: "Password Incorrect"});
-            }
-        });
+      });
     });
 });
 
